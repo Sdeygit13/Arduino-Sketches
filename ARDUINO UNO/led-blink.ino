48443e1a43118177d74088fb37d0ad07cb416a5c
Suvam Dey <deysuvampocom06@gmail.com>
2026-07-20 20:27:40 +0530

    Refactor LED pin usage and adjust blink delay
    Updated LED blink timing and used a variable for pin.

--- ARDUINO UNO/led-blink.ino
+++ ARDUINO UNO/led-blink.ino
@@ -1,12 +1,12 @@
+int led = 7;
 void setup() {
   // put your setup code here, to run once:
-  //Serial.begin(9600);
-  pinMode(7, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(7, HIGH);
-  delay(1000);
-  digitalWrite(7, LOW);
-  delay(1000);
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(400);
 }
