13f5006113cb37567aacb704b052fd75ff5587a2
Suvam Dey <deysuvampocom06@gmail.com>
2026-07-21 20:57:35 +0530

    Add fade-in and fade-out LED control sketch

---
+++ ARDUINO UNO/fade-in-out-analog.ino
@@ -0,0 +1,19 @@
+const int led = 9;
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  Fade in
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(led, i);
+    delay(10);
+  }
+
+  // Fade out
+  for (int i = 255; i >= 0; i--) {
+    analogWrite(led, i);
+    delay(10);
+  }
+}
